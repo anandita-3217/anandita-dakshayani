c426afcd1ad167d1f46227ccde2cc961184dcb46
anandita-3217 <dakshugarimella@gmail.com>
2025-12-27 23:09:13 +0530

    basic changes

---
+++ src/pages/ProjectsPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Learning from '../components/Learning';
+import Projects from '../components/Projects';
+
+function ProjectsPage() {
+  return (
+    <>
+      <Learning />
+      <Projects />
+    </>
+  );
+}
